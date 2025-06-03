4c1a9beb34922978e361c48933b8248b993e97b7
choisujin96 <csjin9783@naver.com>
2025-06-03 17:31:08 +0900

    *계정 및 데이터베이스 만들기, 권한부여*

--- Ex/root.sql
+++ Ex/root.sql
@@ -46,6 +46,7 @@ from user;
 
 
 -- --------------------------------------------------------------
+
 -- #데이터베이스(스키마) 만들기
 create database web_db
 default character set utf8mb4
@@ -64,12 +65,17 @@ show databases;
 use web_db;   
 
 
--- ---------------------------------------------------
+-- --------------------------------------------------------------
 
 -- #권한부여
 -- 'web'@'%'  계정이 web_db (데이타베이스)의 모든 테이블에 모든 권한 부여
 grant all privileges on web_db.* to 'web'@'%' ;
 flush privileges;
 
+-- 'web'@'%' 계정이 hr_db(데이타베이스)의 employees 테이블에 select,insert 권한 부여
+grant select,insert on hr_db.employees to 'web'@'%' ;
+flush privileges;
 
+-- 계정을 생성하거나 권한을 수정한 후, 변경된 권한을 즉시 적용하고자 할 때
+flush privileges;
 
